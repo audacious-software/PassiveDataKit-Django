91d3c78bbd29154863451f243763d63ca719025d
Chris J. Karr <github@audacious-software.com>
2016-01-12 07:07:29 +0000

    Stubbing out CREATE methods for adding content to server

--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import add_data_point
+from .views import add_data_point, add_data_bundle
 
 urlpatterns = [
-    url(r'^add.json$', add_data_point, name='add_data_point'),
+    url(r'^add-point.json$', add_data_point, name='add_data_point'),
+    url(r'^add-bundle.json$', add_data_bundle, name='add_data_bundle'),
 ]
--- views.py
+++ views.py
@@ -1,11 +1,55 @@
+import json
+
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import render
-from django.http import HttpResponseNotAllowed
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import DataPoint
 
+@csrf_exempt
 def add_data_point(request):
+    response = { 'message': 'Data point added successfully.' }
+      
     if request.method == 'CREATE':
-        return HttpResponse(json.dumps(response, indent=2), content_type='application/json', status_code=201)
+        response = HttpResponse(json.dumps(response, indent=2), content_type='application/json', status=201)
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Methods'] = 'CREATE'
+        response['Access-Control-Request-Headers'] = 'Content-Type'
+        response['Access-Control-Allow-Headers'] = 'Content-Type'
+        
+        return response
+    elif request.method == 'OPTIONS':
+        response = HttpResponse('', content_type='text/plain', status=200)
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Methods'] = 'CREATE'
+        response['Access-Control-Request-Headers'] = 'Content-Type'
+        response['Access-Control-Allow-Headers'] = 'Content-Type'
+        
+        return response
+    
+    return HttpResponseNotAllowed(['CREATE'])
+
+
+@csrf_exempt
+def add_data_bundle(request):
+    response = { 'message': 'Data bundle added successfully, and ready for processing.' }
+     
+    if request.method == 'CREATE':
+        response = HttpResponse(json.dumps(response, indent=2), content_type='application/json', status=201)
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Methods'] = 'CREATE'
+        response['Access-Control-Request-Headers'] = 'Content-Type'
+        response['Access-Control-Allow-Headers'] = 'Content-Type'
+        
+        return response
+    elif request.method == 'OPTIONS':
+        response = HttpResponse('', content_type='text/plain', status=200)
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Methods'] = 'CREATE'
+        response['Access-Control-Request-Headers'] = 'Content-Type'
+        response['Access-Control-Allow-Headers'] = 'Content-Type'
+        
+        return response
     
     return HttpResponseNotAllowed(['CREATE'])
 
