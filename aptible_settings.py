3e68bdd005a38e4042d116865f804a7d24fc7099
Chris J. Karr <chris@audacious-software.com>
2016-07-29 04:39:46 +0000

    Fixing static file serving.
    [#3]

--- aptible_settings.py
+++ aptible_settings.py
@@ -83,4 +83,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
