39b0165ea431c3f85dcd9b991e5f2a31cc428b38
Chris J. Karr <chris@audacious-software.com>
2016-07-29 04:32:53 +0000

    WhiteNoise config updates.
    [#3]

--- aptible_settings.py
+++ aptible_settings.py
@@ -82,7 +82,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-
