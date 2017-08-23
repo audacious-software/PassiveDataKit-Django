2c1604b605c9d906872a851eacb7c690c2b9b877
Chris J. Karr <chris@audacious-software.com>
2017-08-23 14:31:40 +0000

    Cleaning up code formatting

--- admin.py
+++ admin.py
@@ -12,8 +12,14 @@ class DataPointVisualizationsAdmin(admin.OSMGeoAdmin):
 class DataPointAdmin(admin.OSMGeoAdmin):
     openlayers_url = 'https://openlayers.org/api/2.13.1/OpenLayers.js'
 
-    list_display = ('source', 'generator_identifier', 'secondary_identifier', 'created', \
-                    'recorded',)
+    list_display = (
+        'source', 
+        'generator_identifier', 
+        'secondary_identifier', 
+        'created',
+        'recorded',
+    )
+    
     list_filter = (
         'created',
         'recorded',
