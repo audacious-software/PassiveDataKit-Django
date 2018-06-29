bb6dbc4c902370dee20c3e6666076eb173f80c19
Chris J. Karr <chris@audacious-software.com>
2018-06-29 01:00:23 -0400

    Initial Google Fit integration

---
+++ generators/pdk_google_fit.py
@@ -0,0 +1,122 @@
+# pylint: disable=line-too-long, no-member
+
+import datetime
+import json
+import time
+
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.utils import timezone
+
+from ..models import DataPoint
+
+def extract_secondary_identifier(properties):
+    if 'reading-type' in properties:
+        return properties['reading-type']
+
+    return None
+
+def generator_name(identifier): # pylint: disable=unused-argument
+    return 'Google Fit'
+
+def compile_frequency_visualization(identifier, points, folder): # pylint: disable=unused-argument
+    now = timezone.now()
+
+    now = now.replace(second=0, microsecond=0)
+
+    remainder = now.minute % 10
+
+    now = now.replace(minute=(now.minute - remainder))
+
+    start = now - datetime.timedelta(days=2)
+
+    points = points.filter(created__lte=now, created__gte=start).order_by('created')
+
+    end = start + datetime.timedelta(seconds=600)
+    point_index = 0
+    point_count = points.count()
+
+    point = None
+
+    if point_count > 0:
+        point = points[point_index]
+
+    timestamp_counts = {}
+
+    keys = []
+
+    while start < now:
+        timestamp = str(time.mktime(start.timetuple()))
+
+        keys.append(timestamp)
+
+        timestamp_counts[timestamp] = 0
+
+        while point is not None and point.created < end and point_index < (point_count - 1):
+            timestamp_counts[timestamp] += 1
+
+            point_index += 1
+
+            point = points[point_index]
+
+        start = end
+        end = start + datetime.timedelta(seconds=600)
+
+    timestamp_counts['keys'] = keys
+
+    with open(folder + '/timestamp-counts.json', 'w') as outfile:
+        json.dump(timestamp_counts, outfile, indent=2)
+
+# def compile_report(generator, sources): # pylint: disable=too-many-locals
+#    timestamp = arrow.get()
+#    filename = tempfile.gettempdir() + '/pdk_export_' + str(timestamp.seconds) + '.' + \
+#               str(timestamp.seconds / 1e6) + '.zip'
+#
+#    with ZipFile(filename, 'w') as export_file:
+#        for secondary_identifier in SECONDARY_FIELDS:
+#            secondary_filename = tempfile.gettempdir() + '/' + generator + '-' + \
+#                                 secondary_identifier + '.txt'
+#
+#            with open(secondary_filename, 'w') as outfile:
+#                writer = csv.writer(outfile, delimiter='\t')
+#
+#                columns = [
+#                    'Source',
+#                    'Created Timestamp',
+#                    'Created Date',
+#                ]
+#
+#                for column in SECONDARY_FIELDS[secondary_identifier]:
+#                    columns.append(column)
+#
+#                writer.writerow(columns)
+#
+#                for source in sources:
+#                    points = DataPoint.objects.filter(source=source, generator_identifier=generator, secondary_identifier=secondary_identifier).order_by('source', 'created') # pylint: disable=no-member,line-too-long
+#
+#                    index = 0
+#                    count = points.count()
+#
+#                    while index < count:
+#                        for point in points[index:(index + 5000)]:
+#                            row = []
+#
+#                            row.append(point.source)
+#                            row.append(calendar.timegm(point.created.utctimetuple()))
+#                            row.append(point.created.isoformat())
+#
+#                            properties = point.fetch_properties()
+#
+#                            for column in SECONDARY_FIELDS[secondary_identifier]:
+#                                if column in properties:
+#                                    row.append(properties[column])
+#                                else:
+#                                    row.append('')
+#
+#                            writer.writerow(row)
+#
+#                        index += 5000
+#
+#            export_file.write(secondary_filename, secondary_filename.split('/')[-1])
+#
+#    return filename
