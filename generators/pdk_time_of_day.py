c7f3e46959a10a1f15a1f854c86ff4a1aa334db0
Chris J. Karr <chris@audacious-software.com>
2019-01-14 23:10:18 +0000

    Exporter updates

--- generators/pdk_time_of_day.py
+++ generators/pdk_time_of_day.py
@@ -123,7 +123,7 @@ def data_table(source, generator):
 
     return render_to_string('generators/pdk_device_time_of_day_table_template.html', context)
 
-def compile_report(generator, sources): # pylint: disable=too-many-locals
+def compile_report(generator, sources, data_start=None, data_end=None): # pylint: disable=too-many-locals
     now = arrow.get()
     filename = tempfile.gettempdir() + '/pdk_export_' + str(now.timestamp) + str(now.microsecond / 1e6) + '.zip'
 
@@ -149,7 +149,15 @@ def compile_report(generator, sources): # pylint: disable=too-many-locals
 
                 writer.writerow(columns)
 
-                points = DataPoint.objects.filter(source=source, generator_identifier=generator).order_by('created')
+                points = DataPoint.objects.filter(source=source, generator_identifier=generator)
+
+                if data_start is not None:
+                    points = points.filter(created__gte=data_start)
+
+                if data_end is not None:
+                    points = points.filter(created__lte=data_end)
+
+                points = points.order_by('created')
 
                 index = 0
                 count = points.count()
