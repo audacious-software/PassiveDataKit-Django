48577d903eeff39c82b2928b98e36bbd4210ab8a
Chris J. Karr <chris@audacious-software.com>
2019-06-11 10:10:38 +0000

    Updated code for populating DataPoint references

--- management/commands/pdk_update_datapoint_references.py
+++ management/commands/pdk_update_datapoint_references.py
@@ -4,12 +4,13 @@ import sys
 
 from django.core.management.base import BaseCommand
 from django.db import transaction
+from django.db.models import Q
 from django.utils import timezone
 
 from ...decorators import handle_lock
-from ...models import DataPoint, CACHED_GENERATOR_DEFINITIONS, CACHED_SOURCE_REFERENCES
+from ...models import DataPoint
 
-PAGE_SIZE = 50000
+PAGE_SIZE = 5000
 
 class Command(BaseCommand):
     help = 'Populates generator and source references on data points missing that metadata by iterating through all points in reverse order of ingestion.'
@@ -18,35 +19,36 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         last_check = timezone.now()
 
-        page = DataPoint.objects.all().order_by('-pk').first().pk
+        query = Q(source_reference_id=None) | Q(generator_definition_id=None)
 
-        while page > 0:
+        remaining = DataPoint.objects.filter(query).count()
+
+        print 'Pending: ' + str(remaining)
+        sys.stdout.flush()
+
+        while remaining > 0:
             with transaction.atomic():
-                for point in DataPoint.objects.filter(pk__gt=(page - PAGE_SIZE), pk__lte=page):
+                for point in DataPoint.objects.filter(query)[:PAGE_SIZE]:
                     updated = False
 
-                    reference = point.source_reference
-
                     if point.source_reference is None:
-                        reference = point.fetch_source_reference(skip_save=True)
+                        point.fetch_source_reference(skip_save=True)
                         updated = True
 
-                    definition = point.generator_definition
-
                     if point.generator_definition is None:
-                        definition = point.fetch_generator_definition(skip_save=True)
+                        point.fetch_generator_definition(skip_save=True)
                         updated = True
 
                     if updated:
                         point.save()
 
-                    if (point.pk % 5000) == 0:
-                        print str(point.pk) + ': ' + str(reference) + ' -- ' + str(definition) + ' -- ' + str(len(CACHED_GENERATOR_DEFINITIONS)) + ' -- ' + str(len(CACHED_SOURCE_REFERENCES))
-                        sys.stdout.flush()
-
-            page = (page - PAGE_SIZE)
             now = timezone.now()
 
-            print 'ELAPSED: ' + str((now - last_check).total_seconds())
+            print 'Elapsed: ' + str((now - last_check).total_seconds())
             sys.stdout.flush()
             last_check = now
+
+            remaining = DataPoint.objects.filter(query).count()
+
+            print 'Pending: ' + str(remaining)
+            sys.stdout.flush()
