3cd48ffc554ab5371d91b95d8900364cedc0dac2
Chris J. Karr <github@audacious-software.com>
2019-06-11 11:24:55 +0000

    Code cleanup

--- generators/pdk_mitmproxy_visit.py
+++ generators/pdk_mitmproxy_visit.py
@@ -1,20 +1,6 @@
 # pylint: disable=line-too-long, no-member
 
-import calendar
-import csv
-import datetime
-import json
-import tempfile
-import time
 import urlparse
-
-import pytz
-
-from django.conf import settings
-from django.template.loader import render_to_string
-from django.utils import timezone
-
-from ..models import DataPoint, install_supports_jsonfield
 
 def generator_name(identifier): # pylint: disable=unused-argument
     return 'PDK mitmproxy Web Visits'
@@ -22,7 +8,7 @@ def generator_name(identifier): # pylint: disable=unused-argument
 def extract_secondary_identifier(properties):
     if 'url' in properties:
         url = urlparse.urlparse(properties['url'])
-        
+
         return url.netloc
 
     return None
