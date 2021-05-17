2cdc750c2301a52f5d7efca10e8957b3715c910e
Chris J. Karr <chris@audacious-software.com>
2021-05-17 18:25:21 -0500

    CircleCI fixes

--- circle_settings.py
+++ circle_settings.py
@@ -1,7 +1,7 @@
 # pylint: skip-file
 
 """
-Settings.py for testing on Travis CI.
+Settings.py for testing on Circle CI.
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -41,7 +41,7 @@ MIDDLEWARE = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = 'passive_data_kit.travis_urls'
+ROOT_URLCONF = 'passive_data_kit.circle_urls'
 
 TEMPLATES = [
     {
