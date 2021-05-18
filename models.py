782f219cc46081849063343465c1722b85834e93
Chris Karr <chris@audacious-software.com>
2021-05-17 21:00:03 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-# pylint: disable=no-member, line-too-long, too-many-lines, super-with-arguments, useless-object-inheritance
+# pylint: disable=no-member, line-too-long, too-many-lines, super-with-arguments, useless-object-inheritance, bad-option-value
 
 from __future__ import print_function
 from __future__ import division
