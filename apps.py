d61297e21833767b79d276a8f6b1d6145ac522c8
Chris J. Karr <chris@audacious-software.com>
2021-10-08 14:11:56 +0000

    Explicitly specifiying ID field type for Django 3

--- apps.py
+++ apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PassiveDataKitConfig(AppConfig):
     name = 'passive_data_kit'
     verbose_name = 'Passive Data Kit'
+    default_auto_field = 'django.db.models.AutoField'
